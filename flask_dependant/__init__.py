062b1e9b43491a7cb773582f4cca8f87fc1366ee
xiaojiang <guwenjiang@baixing.com>
2022-04-11 15:04:10 +0800

    version: 1.0

--- flask_dependant/__init__.py
+++ flask_dependant/__init__.py
@@ -1,7 +1,7 @@
 from flask_dependant.app import FlaskDependant
 from flask_dependant.params import Path, Query, Body, Form, Cookie, Header, Depends
 
-__version__ = "0.0.1"
+__version__ = "1.0"
 
 
 __all__ = [
